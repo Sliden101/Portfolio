dcafeb2d3a8284314d65b2e1b81cbec1e0569053
Sliden101 <chessplayz11@gmail.com>
2022-05-22 14:36:48 +0700

    xd

--- js/renderStars.js
+++ js/renderStars.js
@@ -15,7 +15,7 @@ const presets = [{ // index 0
         rainbow: true,
 
         particles: 100,
-        rate: 40, // lower = faster
+        rate: 50, // lower = faster
         delayAll: 4000,
         delayPer: 5
     },
